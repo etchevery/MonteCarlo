290fa7af5a65633a42d8324890a7e8187f86cca7
MonteCarlo <arnaud.etcheverry@hotmail.com>
2011-02-02 23:41:18 +0100

    update arnaud

--- Tuto_Fichier/Tuto_Fichier/Lumiere.cpp
+++ Tuto_Fichier/Tuto_Fichier/Lumiere.cpp
@@ -11,7 +11,7 @@ Lumiere::~Lumiere(void)
 {
 }
 
-<<<<<<< HEAD
+
 void Lumiere::initFromXML(TiXmlHandle hObj){
 }
 
@@ -20,6 +20,5 @@ void PointLumiere::initFromXML(TiXmlHandle hObj){
 
 void DirectionelleLumiere::initFromXML(TiXmlHandle hObj){
 }
-=======
-void Lumiere::init modifArnaud
->>>>>>> 6c625be408e67b6d080b1b978305b273b79e63f7
+
+void Lumiere::init
